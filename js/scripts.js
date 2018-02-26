b5ec30b47b9a03fb2fb9b639266fed23cd05d394
Rose Oketch <oketchrose@gmail.com>
2018-02-26 10:15:55 +0300

    initial commit for files and folders

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+//user
+$(document).ready(function() {
+  $("form#pingPongForm").submit(function(event) {
+    $("#userOutput").empty();
+    var userInput = parseInt($("input#number").val());
+    var result = pingPongFun(userInput);
+    result.forEach(function(output) {
+    $("#userOutput").append("<li>" + output + "</li>");
+    // $(".#userOutput").animate("url('')")
+    event.preventDefault();
+  });
+  alert("pingpong");
+});
+});
+
+//business
+function pingPongFun(userInput){
+var result = [];
+for (var i = 1; i <= userInput; i++){
+  if ((i % 15) === 0){
+    result.push ("Ping-Pong");
+  } else if ((i % 5) === 0){
+    result.push ("Pong");
+
+  } else if ((i % 3) === 0){
+    result.push ("Ping");
+  } else
+    result.push (i);
+}
+return result;
+}
